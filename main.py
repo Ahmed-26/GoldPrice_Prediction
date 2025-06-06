75a1459b90ab144e3e2d2b00e59277dbf667bade
Ahmed-26 <ahmedrasheed6008@gmail.com>
2025-06-06 16:03:15 +0500

    make format

--- main.py
+++ main.py
@@ -1,11 +1,5 @@
+from src.gold_price_app import GoldPriceApp
 
-from app import GoldPriceApp
-
-def main():
-    # Create an instance of the GoldPriceApp with default parameters
-    app = GoldPriceApp()
-    # Run the Streamlit application
-    app.run()
-
-if __name__ == "__main__":
-    main()
+# Create and run the Streamlit app
+app = GoldPriceApp()
+app.run()
